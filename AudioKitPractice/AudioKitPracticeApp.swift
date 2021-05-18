64774f7fe9fdfa826eb0b7c5f0b98e517b91d13f
miyakawa <haru19970409@gmail.com>
2021-05-18 23:23:27 +0900

    create new project

---
+++ AudioKitPractice/AudioKitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKitPracticeApp.swift
+//  AudioKitPractice
+//
+//  Created by Miyakawa on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct AudioKitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
